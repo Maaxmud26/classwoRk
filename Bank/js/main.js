f8ae391e79af3828696740eb6bfd17157c523248
Maaxmud26 <112488740+Maaxmud26@users.noreply.github.com>
2022-12-06 17:30:38 +0400

    Add files via upload

---
+++ Bank/js/main.js
@@ -0,0 +1,57 @@
+// task2
+
+//      let arr = [1,2,3,4,5,6,7,8,9,10,]
+   
+// function MyFunction(params) {
+//     let arr2 = [];
+//     let arr3 = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 ==0) {
+           
+//             arr2.push(arr[i])
+//         }else {
+// arr3.push(arr[i])
+//         }
+    
+       
+//     }
+//     console.log(arr2)            
+//     console.log(arr3)            
+
+// }
+
+// MyFunction();
+
+
+// task3
+
+
+// function Myfunction(str) {
+
+
+//     let split = str.split("");
+//    let reverse= split.reverse();
+//    for (let i = 0; i< str.length; i++) {
+//     if(reverse[i]==str[i]){
+//         console.log(reverse)
+//         }else{
+//             alert('not palenderom')
+//         }
+//        }
+// }
+// Myfunction("ake");
+// task 1 
+
+
+
+// function Myfunction(str) {
+//     let arr = ["salam" , "necesen" , "yaxsiyam"]
+//     for (let i = 0; i < arr.length; i++) {
+//        if (str==arr[i]) {
+//     console.log(str)
+//        }
+        
+//     }
+// }
+
+// Myfunction("salam")
